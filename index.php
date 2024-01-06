2c23c3769f0fcdb77e4645968028c954a6370982
Oleg Kartashov <mapseam@yandex.ru>
2024-01-06 13:17:22 +0300

    Install PHP_CodeSniffer package and use it

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
+/**
+ * Index.php File Doc Comment
+ * PHP version 8.1.2
 
-echo 'Hello, Hexlet!' . "\n";
+ * @category File
+ * @package  Hexletphp
+ * @author   Oleg Kartashov <mapseam@yandex.ru>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/mapseam/hexlet-php/
+ */
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Hexlet\Php\Runner;
+
+echo 'Hello, Hexlet!' . "\n\n";
+print_r(Runner\run());
--- src/Runner.php
+++ src/Runner.php
@@ -1,12 +1,30 @@
 <?php
+/**
+ * Runner.php File Doc Comment
+ * PHP version 8.1.2
 
+ * @category File
+ * @package  Hexletphp
+ * @author   Oleg Kartashov <mapseam@yandex.ru>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/mapseam/hexlet-php/
+ */
 namespace Hexlet\Php\Runner;
 
+/**
+ * End point for execute code
+ *
+ * @return Collection
+ *
+ * @since 1.0
+ **/
 function run()
 {
-	$collection = collect(['taylor', 'abigail', null])->map(function ($name) {
-		return strtoupper($name);
-	});
+    $collection = collect(['taylor', 'abigail', null])->map(
+        function ($name) {
+            return strtoupper($name);
+        }
+    );
 
-	return $collection;
+    return $collection;
 }
